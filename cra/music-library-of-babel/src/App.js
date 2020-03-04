a6be037d5612df8b77e178759e6708b9948c0060
robalb <alberto.ventafridda@gmail.com>
2020-03-04 13:24:26 +0100

    modified convertbase function to support large radix parameter

--- cra/music-library-of-babel/src/App.js
+++ cra/music-library-of-babel/src/App.js
@@ -3,13 +3,13 @@ import logo from './logo.svg';
 import {Flow} from 'vexflow';
 
 import getNotesPermutations from './scripts/getNotesPermutations.js';
-import { convertBase2, convertBase } from './scripts/convertBase.js';
+import { convertBaseR, convertBase } from './scripts/convertBase.js';
 
 console.log("--BEGIN TESTING--")
 
 function test(string, baseFrom, baseTo, radix){
-  let converted = convertBase(string, baseFrom, baseTo, radix)
-  let reconverted = convertBase(converted, baseTo, baseFrom, radix)
+  let converted = convertBaseR(string, baseFrom, baseTo, radix)
+  let reconverted = convertBaseR(converted, baseTo, baseFrom, radix)
   console.log({
     string: string,
     converted: converted,
@@ -18,99 +18,107 @@ function test(string, baseFrom, baseTo, radix){
   })
 }
 
+let radix = getNotesPermutations()
+console.log(radix)
 
-test("5a2a9c826c75045be9ba8fbffc80c6f25a2a9c826c75045be9ba8fbffc80c6f2", 16, 54, '')
-test("5a2a9c826c75045be9ba8fbffc80c6f25a2a9c826c75045be9ba8fbffc80c6f2", 16, 3, '')
+test("c/4/16/1/0-c/4/8/0/1-d/4/16/0/0", radix.length, 10, radix)
+test("56623104", 10, radix.length, radix)
+test("566231040000", 10, radix.length, radix)
+test("ffffff", 16, radix.length, radix)
+test("5a2a9c826c75045be9ba8fbffc80c6f25a2a9c826c75045be9ba8fbffc80c6f2", 16, 3, radix)
 
 console.log("--END TESTING--")
 
-function App() {
-  let [index, setIndex] = React.useState(1);
-  let [base1, setBase1] = React.useState(1);
-  let [base2, setBase2] = React.useState(1);
-  let [toDecimal, setToDecimal] = React.useState(1);
-
-  let noteContainerRef = React.createRef();
-
-  React.useEffect(()=>{
-    let div = document.getElementById("boo")
-    let VF = Flow;
-    let renderer = new VF.Renderer(noteContainerRef.current, VF.Renderer.Backends.SVG);
-    // Configure the rendering context.
-    renderer.resize(420, 120);
-    let context = renderer.getContext();
-    context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
-
-    // Create a stave of width 400 at position 10, 40 on the canvas.
-    let stave = new VF.Stave(8, 0, 400);
-    stave.setEndBarType(VF.Barline.type.DOUBLE);
-    // Add a clef and time signature.
-    stave.addClef("treble").addTimeSignature("4/4");
-    // Connect it to the rendering context and draw!
-    stave.setContext(context).draw();
-
-
-    let rndIndex = Math.floor(Math.random()*300)
-    let notes = getNotesPermutations().slice(rndIndex, rndIndex+10)
-    console.log(notes)
-    notes = notes.map(note => {
-      //parse note format
-      let [letterAndAcc, octave, duration, isDotted, isRest] = note.split("/")
-      if(isRest === "1") duration += "r";
-      //generate note
-      let generated = new VF.StaveNote({
-        clef: "treble",
-        keys: [`${letterAndAcc}/${octave}`],
-        duration: duration
-      });
-      if(isDotted === "1"){
-        generated.addDotToAll();
-      }
-
-      return generated;
-    });
-
-    let beams = VF.Beam.generateBeams(notes);
-    VF.Formatter.FormatAndDraw(context, stave, notes);
-    beams.forEach(function(b) {b.setContext(context).draw()})
-
-
-  });
-  // }, [index]);
-
-  function updateData(value){
-    if(value>1){
-      setIndex(value);
-      console.log(index)
-      value = String(value)
-      let baseX = convertBase(value, 10, 20)
-      console.log(baseX)
-      console.log(convertBase(String(baseX), 20, 10))
-    }
-  }
-  return (
-    <div className="App">
-      <div className="input-group centered">
-        <input id="a11y-input3"
-        aria-labelledby="#a11y-input3"
-        tabIndex="1"
-        type="number"
-        onChange = {e=>updateData(parseInt(e.target.value))}
-        required/>
-        <span className="bar"></span>
-        <label htmlFor="a11y-input3">index</label>
-      </div>
-
-      <h2><span>decimal</span> {index}</h2>
-      <h2><span>tobase</span> {base1}</h2>
-      <h2><span>tobase</span> {base2}</h2>
-      <h2><span>back to decimal</span> {toDecimal}</h2>
-
-      <div className="noteContainer" ref={noteContainerRef}></div>
-
-
-    </div>
-  );
+function App(){
+  return <p>check the console</p>
 }
+//function App() {
+//  let [index, setIndex] = React.useState(1);
+//  let [base1, setBase1] = React.useState(1);
+//  let [base2, setBase2] = React.useState(1);
+//  let [toDecimal, setToDecimal] = React.useState(1);
+
+//  let noteContainerRef = React.createRef();
+
+//  React.useEffect(()=>{
+//    let div = document.getElementById("boo")
+//    let VF = Flow;
+//    let renderer = new VF.Renderer(noteContainerRef.current, VF.Renderer.Backends.SVG);
+//    // Configure the rendering context.
+//    renderer.resize(420, 120);
+//    let context = renderer.getContext();
+//    context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+//    // Create a stave of width 400 at position 10, 40 on the canvas.
+//    let stave = new VF.Stave(8, 0, 400);
+//    stave.setEndBarType(VF.Barline.type.DOUBLE);
+//    // Add a clef and time signature.
+//    stave.addClef("treble").addTimeSignature("4/4");
+//    // Connect it to the rendering context and draw!
+//    stave.setContext(context).draw();
+
+
+//    let rndIndex = Math.floor(Math.random()*300)
+//    let notes = getNotesPermutations().slice(rndIndex, rndIndex+10)
+//    console.log(notes)
+//    notes = notes.map(note => {
+//      //parse note format
+//      let [letterAndAcc, octave, duration, isDotted, isRest] = note.split("/")
+//      if(isRest === "1") duration += "r";
+//      //generate note
+//      let generated = new VF.StaveNote({
+//        clef: "treble",
+//        keys: [`${letterAndAcc}/${octave}`],
+//        duration: duration
+//      });
+//      if(isDotted === "1"){
+//        generated.addDotToAll();
+//      }
+
+//      return generated;
+//    });
+
+//    let beams = VF.Beam.generateBeams(notes);
+//    VF.Formatter.FormatAndDraw(context, stave, notes);
+//    beams.forEach(function(b) {b.setContext(context).draw()})
+
+
+//  });
+//  // }, [index]);
+
+//  function updateData(value){
+//    if(value>1){
+//      setIndex(value);
+//      console.log(index)
+//      value = String(value)
+//      let baseX = convertBase(value, 10, 20)
+//      console.log(baseX)
+//      console.log(convertBase(String(baseX), 20, 10))
+//    }
+//  }
+//  return (
+//    <div className="App">
+//      <div className="input-group centered">
+//        <input id="a11y-input3"
+//        aria-labelledby="#a11y-input3"
+//        tabIndex="1"
+//        type="number"
+//        onChange = {e=>updateData(parseInt(e.target.value))}
+//        required/>
+//        <span className="bar"></span>
+//        <label htmlFor="a11y-input3">index</label>
+//      </div>
+
+//      <h2><span>decimal</span> {index}</h2>
+//      <h2><span>tobase</span> {base1}</h2>
+//      <h2><span>tobase</span> {base2}</h2>
+//      <h2><span>back to decimal</span> {toDecimal}</h2>
+
+//      <div className="noteContainer" ref={noteContainerRef}></div>
+
+
+//    </div>
+//  );
+//}
 
 export default App;
--- cra/music-library-of-babel/src/scripts/convertBase.js
+++ cra/music-library-of-babel/src/scripts/convertBase.js
@@ -1,32 +1,5 @@
 // https://www.danvk.org/hex2dec.html
 
-let convertBase2 = (value, from_base, to_base, range) => {
-    if(!range){
-      range = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/".split("");
-    }
-    if (from_base < 2 || from_base > range.length)
-        throw new RangeError(`convertBase() from_base argument must be between 2 and ${range.length}`);
-    if (to_base < 2 || to_base > range.length)
-        throw new RangeError(`convertBase() to_base argument must be between 2 and ${range.length}`);
-    let from_range = range.slice(0, from_base);
-    let to_range = range.slice(0, to_base);
-    
-    let dec_value = value.split("").reverse().reduce((carry, digit, index) => {
-        let fromIndex = from_range.indexOf(digit);
-        if (fromIndex === -1)
-            throw new Error(`Invalid digit ${digit} for base ${from_base}.`);
-        return carry + fromIndex * Math.pow(from_base, index);
-    }, 0);
-    
-    let new_value = "";
-    while (dec_value > 0) {
-        new_value = to_range[dec_value % to_base] + new_value;
-        dec_value = (dec_value - dec_value % to_base) / to_base;
-    }
-    return new_value || "0";
-};
-
-
 // https://stackoverflow.com/a/55011290
 function convertBase(str, fromBase, toBase) {
 
@@ -93,24 +66,83 @@ function convertBase(str, fromBase, toBase) {
 }
 
 
-function convertBase10toX(value, to_base){
-  let radix = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  let Z = [];
-  let M = -1;
-  for (let i = value; 0 < i; i = Math.floor(i / to_base)) { 
-    if(i % to_base >= 10) { 
-      Z.push(radix[i % to_base - 10]);
-    } else { 
-      Z.push(i % to_base);
-    } 
-    M = M + 1;
-  } 
-  console.log(Z)
-  return Z.reverse().join(" - ")
-  // for (j = M; j >= 0; j--) {
-  //   document.write(Z[j]);
-  // } 
+
+function convertBaseR(str, fromBase, toBase, radix) {
+    
+    const DIGITS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/".split('');
+    const LARGEDIGITS = radix || "";
+
+    const add = (x, y, base) => {
+        let z = [];
+        const n = Math.max(x.length, y.length);
+        let carry = 0;
+        let i = 0;
+        while (i < n || carry) {
+            const xi = i < x.length ? x[i] : 0;
+            const yi = i < y.length ? y[i] : 0;
+            const zi = carry + xi + yi;
+            z.push(zi % base);
+            carry = Math.floor(zi / base);
+            i++;
+        }
+        return z;
+    }
+
+    const multiplyByNumber = (num, x, base) => {
+        if (num < 0) return null;
+        if (num == 0) return [];
+
+        let result = [];
+        let power = x;
+        while (true) {
+            num & 1 && (result = add(result, power, base));
+            num = num >> 1;
+            if (num === 0) break;
+            power = add(power, power, base);
+        }
+
+        return result;
+    }
+
+    const parseToDigitsArray = (str, base) => {
+        let usedDigits = DIGITS;
+        let splitParam = '';
+        if(base > DIGITS.length){
+          usedDigits = LARGEDIGITS;
+          splitParam = '-';
+        }
+        const digits = str.split(splitParam);
+        let arr = [];
+        for (let i = digits.length - 1; i >= 0; i--) {
+            const n = usedDigits.indexOf(digits[i])
+            if (n == -1) return null;
+            arr.push(n);
+        }
+        return arr;
+    }
+
+    const digits = parseToDigitsArray(str, fromBase);
+    if (digits === null) return null;
+
+    let outArray = [];
+    let power = [1];
+    for (let i = 0; i < digits.length; i++) {
+        digits[i] && (outArray = add(outArray, multiplyByNumber(digits[i], power, toBase), toBase));
+        power = multiplyByNumber(fromBase, power, toBase);
+    }
+
+    let out = '';
+    for (let i = outArray.length - 1; i >= 0; i--)
+        if(toBase > DIGITS.length){
+          let divisor = i > 0 ? '-' : '';
+          out += LARGEDIGITS[outArray[i]] + divisor;
+        } else {
+          out += DIGITS[outArray[i]];
+        }
+
+    return out;
 }
 
 
-export {convertBase, convertBase2};
+
+export {convertBase, convertBaseR};
